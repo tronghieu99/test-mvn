6c65db8a2cc5375b1b4375ba206992e2ab7cc17f
hieu99 <hieutt@cozwork>
2020-10-19 13:29:30 +0700

    update code

--- src/main/java/com/hieutt/aaa/Cat.java
+++ src/main/java/com/hieutt/aaa/Cat.java
@@ -2,6 +2,28 @@ package com.hieutt.aaa;
 
 public class Cat extends Animal {
 
+    public int age;
+    public String name;
+
+    public Cat() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void keu() {
         System.out.println("cat...");
     }
